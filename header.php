b84375c600b403eb845b38c041ed2c1abe02fe39
vishu87 <vishu.iitd@gmail.com>
2016-12-06 22:02:19 +0530

    header fix

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
-	<title><?php bloginfo('name'); ?><?php wp_title('||'); ?></title>
+	<title><?php wp_title('||'); ?></title>
 	<?php wp_head(); ?>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
